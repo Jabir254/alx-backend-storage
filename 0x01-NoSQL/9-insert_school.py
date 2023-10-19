ea9d46575cab750116ab7b09d0d73e35575447c9
jabir254 <jabirkullow@gmail.com>
2023-10-19 20:24:43 +0300

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''inserts a new document in a collection'''
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert a documents into a collection
+    """
+    data = mongo_collection.insert_one(kwargs)
+    return data.inserted_id
